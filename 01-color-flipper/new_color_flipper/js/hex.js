16fbfc50ae81752565666486d7b08597d8b7d406
yogashri97 <yogashri07@gmail.com>
2023-12-15 17:50:14 +0530

    new

--- 01-color-flipper/new_color_flipper/js/hex.js
+++ 01-color-flipper/new_color_flipper/js/hex.js
@@ -1,4 +1,4 @@
-btn-groupbtn-groupbtn-groupbtn-groupbtn-groupbtn-groupbtn-groupbtn-groupconst hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn1 = document.getElementById("btn1");
 const color = document.querySelector(".color");
 
