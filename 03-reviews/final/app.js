ac1e1d900f8e1c17c04b00380ab4598d0a63071b
imjusthariharan <hariharansbsni@gmail.com>
2023-12-29 18:52:27 +0530

    reviwes is completed

--- 03-reviews/final/app.js
+++ 03-reviews/final/app.js
@@ -29,56 +29,42 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
-// select items
-const img = document.getElementById('person-img');
+
+/****** link variables ********/
+const pic = document.getElementById('person-img');
 const author = document.getElementById('author');
 const job = document.getElementById('job');
 const info = document.getElementById('info');
-
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
-
-// set starting item
-let currentItem = 0;
-
-// load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+/****** link buttons ********/
+const nxt = document.querySelector('.next-btn');
+const pre = document.querySelector('.prev-btn');
+const rand = document.querySelector('.random-btn');
+let record_Id = 0;
+/****** event listners ********/
+nxt.addEventListener('click', function () {
+  record_Id++;
+  if (record_Id > reviews.length - 1) {
+    record_Id = 0;
+  }
+  showHisFeedback(record_Id);
 });
-
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+pre.addEventListener('click', function () {
+  record_Id--;
+  if (record_Id < 0) {
+    record_Id = reviews.length - 1;
+  }
+  showHisFeedback(record_Id);
+});
+rand.addEventListener('click', function () {
+  record_Id = Math.floor(Math.random() * reviews.length);
+  showHisFeedback(record_Id);
+  console.log(record_Id);
+})
+/** show  this person **/
+function showHisFeedback(id_to_show) {
+  const articleVal = reviews[id_to_show];
+  pic.src = articleVal.img;
+  author.textContent = articleVal.name;
+  job.textContent = articleVal.job;
+  info.textContent = articleVal.text;
 }
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
-
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
